b7881e7f6e4bbff240416a1c87b91ecfcb2bcb0b
Marcelo Oliveira <marcelo.oliveira@alura.com.br>
2020-01-02 15:12:35 -0300

    Final aula 4

--- 6-listas.js
+++ 6-listas.js
@@ -3,8 +3,6 @@ console.log(`Trabalhando com listas`);
 // const saoPaulo = `São Paulo`;
 // const rioDeJaneiro = `Rio de Janeiro`;
 
-let new = 2;
-
 const listaDeDestinos = new Array(
     `Salvador`,
     `São Paulo`,
---
+++ 7-condicionais.js
@@ -0,0 +1,46 @@
+console.log(`Trabalhando com condicionais`);
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+
+const idadeComprador = 18;
+const estaAcompanhada = false;
+const temPassagemComprada = true;
+
+console.log("Destinos possíveis:");
+console.log(listaDeDestinos);
+
+// if (idadeComprador >= 18) {
+//     console.log("Comprador maior de idade");
+//     listaDeDestinos.splice(1, 1); //removendo item
+// } else if (estaAcompanhada == true) {
+//     console.log("Comprador está acompanhado");
+//     listaDeDestinos.splice(1, 1); //removendo item
+// } else {
+//     console.log("Não é maior de Idade e não posso vender");
+// }
+
+if (idadeComprador >= 18 || estaAcompanhada == true) {
+    console.log("Boa Viagem!!");
+    listaDeDestinos.splice(2, 1); //removendo item
+} else {
+    console.log("Não é maior de Idade e não posso vender");
+}
+
+console.log("Embarque: \n\n")
+if(idadeComprador >= 18 && temPassagemComprada){
+    console.log("Boa viagem");
+}else{
+    console.log("Você não pode embarcar");
+}
+
+
+console.log(listaDeDestinos);
+
+// console.log(idadeComprador > 18);
+// console.log(idadeComprador < 18);
+// console.log(idadeComprador <= 18);
+// console.log(idadeComprador >= 18);
+// console.log(idadeComprador == 18);
