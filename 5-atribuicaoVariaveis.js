15a54f99f1d240775b4ce60b87dbd4a74273aafb
Marcelo Oliveira <marcelo.oliveira@alura.com.br>
2020-01-02 15:12:08 -0300

    Final aula 3

--- 5-atribuicaoVariaveis.js
+++ 5-atribuicaoVariaveis.js
@@ -1,14 +1,20 @@
 console.log("Trabalhando com atribuição de variáveis");
-const idade = 29;
+
 const primeiroNome = "Ricardo";
 const sobrenome = "Bugan";
 
 //console.log(nome + " " + sobrenome)
-console.log(primeiroNome, sobrenome)
+console.log(primeiroNome, sobrenome);
 console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
 
-let contador = 0
-contador = contador +1
+let contador = 0;
+contador = contador +1;
+
 const nomeCompleto = primeiroNome + sobrenome;
 console.log(nomeCompleto);
-nomeCompleto =2;
+
+let idade; //declarando variável
+idade = 26; //atribuindo valor
+idade = idade+1;
+console.log(idade);
+
---
+++ 6-listas.js
@@ -0,0 +1,23 @@
+console.log(`Trabalhando com listas`);
+// const salvador = `Salvador`;
+// const saoPaulo = `São Paulo`;
+// const rioDeJaneiro = `Rio de Janeiro`;
+
+let new = 2;
+
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+
+listaDeDestinos.push(`Curitiba`) // adicionando um item na lista
+console.log("Destinos possíveis:");
+//console.log(salvador, saoPaulo, rioDeJaneiro)
+console.log(listaDeDestinos);
+
+listaDeDestinos.splice(1,1);
+console.log(listaDeDestinos);
+
+
+console.log(listaDeDestinos[1], listaDeDestinos[0]);
