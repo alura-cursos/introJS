be04b65ccdc3a3b19f96242d29fbb08daf6a643e
Marcelo Oliveira <marcelo.oliveira@alura.com.br>
2020-01-02 15:11:11 -0300

    Final aula 1

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,12 @@
+console.log("Meu Primeiro programa: trabalhando com variáveis");
+
+const idade = 26;
+
+console.log("idade");
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+
+const idadeSomada = idade+2;
+console.log(idadeSomada);
